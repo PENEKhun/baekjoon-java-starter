267b4a06da76b4b54bc56c6043eabce0d679396c
penekhun <penekhun@gmail.com>
2023-12-25 23:39:20 +0900

    remove unused code

--- src/main/java/kr/huni/problem_parser/TestCase.java
+++ src/main/java/kr/huni/problem_parser/TestCase.java
@@ -2,11 +2,4 @@ package kr.huni.problem_parser;
 
 public record TestCase(String input, String output) {
 
-  @Override
-  public String toString() {
-    return "TestCase{" +
-        "input='" + input + '\'' +
-        ", output='" + output + '\'' +
-        '}';
-  }
 }
