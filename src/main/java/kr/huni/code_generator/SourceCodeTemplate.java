6a2b64773af3ab35e566531202f3ba8f46f15d93
penekhun <penekhun@gmail.com>
2023-12-26 05:20:10 +0900

    TestHelper 코드 템플릿 수정
    틀린 케이스에 대한 출력만 + 출력할때 색깔 넣고 이쁘게 출력

--- src/main/java/kr/huni/code_generator/SourceCodeTemplate.java
+++ src/main/java/kr/huni/code_generator/SourceCodeTemplate.java
@@ -55,12 +55,10 @@ public class SourceCodeTemplate {
               this.expectedOutput = expectedOutput;
             }
           }
-              
         """);
 
     code.append("""
-        public static void main(String[] args){
-                  
+        public static void main(String[] args) {
           TestCase[] testCases = new TestCase[]{
           """);
 
@@ -79,44 +77,68 @@ public class SourceCodeTemplate {
     }
 
     code.append("""
-            };
-                        
-            for (TestCase testCase : testCases) {
-              System.out.println("========================================");
-              System.out.println("입력 값: " + testCase.input);
-              System.out.println("기대 값: " + testCase.expectedOutput);
+              };
               
-              ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-              PrintStream printStream = new PrintStream(outputStream);
-              PrintStream printOut = System.out;
-              System.setOut(printStream);
-              System.setIn(new ByteArrayInputStream(testCase.input.getBytes()));
+              int passedCases = 0;
               
-              try {
-                Main.main(new String[0]);
-              } catch (Exception e) {
-                System.out.println("NG");
-                System.out.println("실행 중 에러가 발생했습니다.");
-                System.out.println(e.getMessage());
-                continue;
-              }
+              for (int i = 0; i < testCases.length; i++) {
+                TestCase testCase = testCases[i];
+                
+                System.out.println("========================================");
+                System.out.println("입력 값: " + testCase.input);
+                System.out.println("기대 값: " + testCase.expectedOutput);
+                
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                PrintStream printOut = System.out;
+                System.setOut(printStream);
+                System.setIn(new ByteArrayInputStream(testCase.input.getBytes()));
+                
+                try {
+                  Main.main(new String[0]);
+                } catch (Exception e) {
+                  printFail(i + 1, testCase, e.getMessage());
+                  continue;
+                }
 
-              String output = outputStream.toString();
-              System.setOut(printOut);
-              if (output.equals(testCase.expectedOutput)) {
-                System.out.println("OK");
-              } else {
-                System.out.println("NG");
-                System.out.println("실제 값 : " + output);
+                String output = outputStream.toString();
+                System.setOut(printOut);
+                if (output.equals(testCase.expectedOutput)) {
+                  passedCases++;
+                  continue;
+                } else {
+                  printFail(i + 1, testCase,
+                      red(""\"
+                          [실제 값]
+                          %s
+                          ""\".formatted(output)));
+                }
+                
               }
-
-              System.setIn(System.in);
-              System.out.println("========================================");
+              
+              System.out.println("테스트 완료 (" + passedCases + " / " + testCases.length + ")");
+              if (passedCases == testCases.length) {
+                System.out.println("주어진 케이스에 대해 잘 동작하고 있습니다.");
+              }
+            }
+            
+            public static void printFail(int caseNumber, TestCase testCase, String message) {
+              System.out.printf(""\"
+                  ====== %s ======%n
+                  [입력 값]
+                  %s
+                  [기대 값]
+                  %s%n
+                  ""\", red(caseNumber + " 번째 케이스 실패 "), caseNumber, testCase.input, testCase.expectedOutput);
+              
+              System.out.println(message);
+            }
+            
+            public static String red(String message) {
+              return "\\u001B[31m%s\\u001B[0m".formatted(message);
             }
           }
-        }
-                      
-              """);
+        """);
 
     return code.toString();
   }
