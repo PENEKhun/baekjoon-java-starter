ce88fd4a8486a4ca445532f890a0501bbd479870
penekhun <penekhun@gmail.com>
2023-12-28 09:20:52 +0900

    test 코드 생성부분 테스트 케이스 추가

--- src/test/java/kr/huni/code_generator/SourceCodeTemplateTest.java
+++ src/test/java/kr/huni/code_generator/SourceCodeTemplateTest.java
@@ -26,13 +26,33 @@ class SourceCodeTemplateTest {
   }
 
   @Test
-  @DisplayName("test 메소드가 존재하는 소스코드는 컴파일이 잘 된다.")
+  @DisplayName("테스트케이스가 있을때 생성된 TestHelper.java는 컴파일이 잘 된다.")
   void test_syntax_fine() throws IOException {
     // given
     ArrayList<TestCase> testCases = new ArrayList<>();
     testCases.add(new TestCase("1 2", "3"));
 
     String testCode = SourceCodeTemplate.getTestCode(testCases);
+    testCode += """
+            class Main {
+              public static void main(String[] args) {
+                // do nothing
+              }
+            }
+        """;
+
+    // when
+    boolean working = DynamicCodeCompileSupporter.checkCompileWorking(testCode);
+
+    // then
+    Assertions.assertTrue(working);
+  }
+
+  @Test
+  @DisplayName("테스트 케이스가 없을때 생성된 TestHelper.java도 컴파일이 잘 된다.")
+  void test_syntax_fine_with_no_case() throws IOException {
+    // given
+    String testCode = SourceCodeTemplate.getTestCode(new ArrayList<>());
     testCode += """
             class Main {
               public static void main(String[] args) {
