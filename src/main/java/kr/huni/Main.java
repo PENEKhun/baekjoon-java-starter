25fc786d109e447011fc60c6c68a7b3ca2ea766d
MoonSeonghun <penekhun@gmail.com>
2023-12-28 10:44:02 +0900

    0.1.5 version update (#9)
    # Document
    - 이슈 템플릿 수정
    - README 문서 수정

--- src/main/java/kr/huni/Main.java
+++ src/main/java/kr/huni/Main.java
@@ -1,13 +1,12 @@
 package kr.huni;
 
-import java.io.IOException;
 import java.util.Scanner;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
-  public static void main(String[] args) throws IllegalArgumentException, IOException {
+  public static void main(String[] args) throws IllegalArgumentException {
     Scanner scanner = new Scanner(System.in);
     log.info("백준 문제 번호를 입력해 주세요: ");
     int number = scanner.nextInt();
