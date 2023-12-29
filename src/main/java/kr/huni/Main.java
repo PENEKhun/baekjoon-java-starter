61c95aeeaf1694730b216b0d02fe4bfba8af2f9f
penekhun <penekhun@gmail.com>
2023-12-29 13:51:28 +0900

    feat: 프로그램 실행시 바로 설정파일 생성되도록 수정

--- src/main/java/kr/huni/Main.java
+++ src/main/java/kr/huni/Main.java
@@ -1,12 +1,15 @@
 package kr.huni;
 
 import java.util.Scanner;
+import kr.huni.user_configuration.UserConfigurationLoader;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) throws IllegalArgumentException {
+    UserConfigurationLoader.getInstance();
+    
     Scanner scanner = new Scanner(System.in);
     log.info("백준 문제 번호를 입력해 주세요: ");
     int number = scanner.nextInt();
