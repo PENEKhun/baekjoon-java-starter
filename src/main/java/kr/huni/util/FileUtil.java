dd515e005680aaeb1f18bad419ca1900e7ac4aea
penekhun <penekhun@gmail.com>
2023-12-29 13:39:34 +0900

    feat: 파일 존재시 덮어씌울지 물어보는 기능 추가

--- src/main/java/kr/huni/util/FileUtil.java
+++ src/main/java/kr/huni/util/FileUtil.java
@@ -1,5 +1,6 @@
 package kr.huni.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,6 +18,16 @@ public class FileUtil {
 
   private void writeToFile(File directory, String fileName, String code) throws IOException {
     File file = new File(directory, fileName);
+    if (file.exists()) {
+      log.warn("{}/{}가 이미 존재합니다. 덮어 씌우시겠습니까? (y, n)", directory.getName(), fileName);
+      BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+      String answer = reader.readLine();
+      if (answer.equals("y")) {
+        log.info("기존 파일을 덮어 씌웁니다.");
+      } else {
+        return;
+      }
+    }
 
     try (FileWriter fileWriter = new FileWriter(file)) {
       fileWriter.write(code);
