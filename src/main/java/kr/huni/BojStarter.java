88746f42e0cb62563a82f3321f1ffd1b2a39f68f
penekhun <penekhun@gmail.com>
2024-01-01 00:51:59 +0900

    refactor: stater application 설계 계선
    DI, method extract

--- src/main/java/kr/huni/BojStarter.java
+++ src/main/java/kr/huni/BojStarter.java
@@ -1,27 +1,36 @@
 package kr.huni;
 
 import java.io.IOException;
-import kr.huni.code.generator.CodeGenerator;
-import kr.huni.code.runner.IntellijRunManager;
-import kr.huni.problem_parser.ProblemParser;
+import kr.huni.code_generator.CodeGenerator;
+import kr.huni.code_generator.GeneratedCode;
+import kr.huni.code_runner.CodeOpenManager;
+import kr.huni.file_generator.JavaSourceCodeFile;
+import kr.huni.os.OperatingSystem;
+import kr.huni.problem_parser.BaekjoonProblemParser;
+import kr.huni.problem_parser.Problem;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class BojStarter {
 
-  private final int problemNumber;
+  private final CodeOpenManager codeOpenManager;
+  private final JavaSourceCodeFile fileUtil;
+  private final CodeGenerator codeGenerator;
+  private final BaekjoonProblemParser problemParser;
 
-  public BojStarter(int problemNumber) {
-    this.problemNumber = problemNumber;
+  public void run(final int problemNumber) {
+    Problem problem = problemParser.parse(problemNumber);
+    GeneratedCode generatedCode = codeGenerator.generate(problem);
+
+    createSrcFile(problem, generatedCode);
+    openSourceCodeWithIde(problem);
   }
 
-  public void start() {
-    ProblemParser parser = new ProblemParser(this.problemNumber);
-    CodeGenerator generator = new CodeGenerator(parser.getProblem());
-    generator.generate();
-    IntellijRunManager runManager = new IntellijRunManager();
+  private void openSourceCodeWithIde(Problem problem) {
     try {
-      runManager.run(parser.getProblem().getDirectory());
+      codeOpenManager.run(problem.getSourceCodeDirectory(), OperatingSystem.getOperatingSystem());
     } catch (IOException e) {
       log.error("""
           [ERROR]
@@ -29,7 +38,17 @@ public class BojStarter {
           직접 IntelliJ IDEA를 실행해서 프로젝트를 열어주세요.
           생성된 프로젝트 경로 : {}
           %n
-          """, parser.getProblem().getDirectory(), e);
+          """, problem.getSourceCodeDirectory(), e);
+    }
+  }
+
+  private void createSrcFile(Problem problem, GeneratedCode generatedCode) {
+    try {
+      fileUtil.createDirectory(problem.getSourceCodeDirectory());
+      fileUtil.write(problem.getSourceCodeDirectory(), generatedCode.mainCode(),
+          generatedCode.testCode());
+    } catch (IOException e) {
+      log.error("소스코드 파일 또는 디렉토리 생성에 실패했습니다.", e);
     }
   }
 }
--- src/main/java/kr/huni/Main.java
+++ src/main/java/kr/huni/Main.java
@@ -1,22 +1,33 @@
 package kr.huni;
 
 import java.util.Scanner;
+import kr.huni.code_generator.JavaCodeGenerator;
+import kr.huni.code_runner.IdeaCodeOpenManager;
+import kr.huni.file_generator.JavaSourceCodeFile;
+import kr.huni.problem_parser.BaekjoonProblemParser;
+import kr.huni.problem_parser.JsoupWebParser;
 import kr.huni.user_configuration.UserConfigurationLoader;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class Main {
 
   public static void main(String[] args) throws IllegalArgumentException {
     UserConfigurationLoader.getInstance();
-    
+
     Scanner scanner = new Scanner(System.in);
     log.info("백준 문제 번호를 입력해 주세요: ");
-    int number = scanner.nextInt();
+    int problemNumber = scanner.nextInt();
 
-    BojStarter bojStarter = new BojStarter(number);
-    bojStarter.start();
-
+    BojStarter bojStarter = new BojStarter(
+        new IdeaCodeOpenManager(),
+        new JavaSourceCodeFile(),
+        new JavaCodeGenerator(),
+        new BaekjoonProblemParser(new JsoupWebParser(problemNumber))
+    );
+    bojStarter.run(problemNumber);
     scanner.close();
   }
 
