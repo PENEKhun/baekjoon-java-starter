76425befe5950fb5cd71ae9bc28fd0ef850100db
penekhun <penekhun@gmail.com>
2024-01-01 01:55:34 +0900

    fix: logical 한 문제로 파일이 생성되지 않는 오류

--- src/main/java/kr/huni/file_generator/SourceCodeFile.java
+++ src/main/java/kr/huni/file_generator/SourceCodeFile.java
@@ -21,50 +21,44 @@ public interface SourceCodeFile {
    * @param sourceCode 소스코드 내용
    * @param testCode   테스트 코드 내용
    * @throws IOException 파일 생성 실패
-   * @implSpec 해당 메서드안에서 {@link #writeToFile(String, String, String)}를 통해 알고리즘을 구현할 소스코드 파일과 테스트 코드
-   * 파일을 생성해야합니다.
+   * @implSpec 해당 메서드안에서 필요한 하위 폴더를 생성하고, {@link #writeToFile(File, String, String)}를 통해 알고리즘을 구현할
+   * 소스코드 파일과 테스트 코드 파일을 생성해야합니다.
    */
   void write(String directory, String sourceCode, String testCode) throws IOException;
 
   /**
    * 파일을 생성하고, 내용을 채웁니다.
    *
-   * @param directory  파일이 생성될 디렉토리
+   * @param srcDir     소스코드를 저장할 위치가 담긴 객체
    * @param fileName   파일 이름
    * @param sourceCode 파일 내용
-   * @throws IOException 파일 생성 실패
    */
-  default void writeToFile(String directory, String fileName, String sourceCode)
-      throws IOException {
-    File file = new File(directory, fileName);
-
+  default void writeToFile(File srcDir, String fileName, String sourceCode) {
+    File file = new File(srcDir, fileName);
     if (file.exists()) {
-      System.out.printf("%s/%s가 이미 존재합니다. 새롭게 덮어 씌우시겠습니까? (y, n): ", directory, fileName);
+      System.out.printf("%s/%s가 이미 존재합니다. 새롭게 덮어 씌우시겠습니까? (y, n): ", srcDir.getAbsoluteFile(),
+          fileName);
       BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
       try {
         String answer = reader.readLine();
         if (!answer.equals("y")) {
+          System.out.println("파일을 덮어 씌웁니다.");
+        } else {
           System.out.println("파일을 덮어 씌우지 않고 진행합니다.");
           return;
-        }
-        System.out.println("파일을 덮어 씌웁니다.");
-        try (FileWriter fileWriter = new FileWriter(file)) {
-          fileWriter.write(sourceCode);
         }
       } catch (IOException e) {
         System.out.println("입력이 잘못 되었습니다. 파일을 덮어 씌우지 않고 진행합니다.");
       }
     }
 
+    try (FileWriter fileWriter = new FileWriter(file)) {
+      fileWriter.write(sourceCode);
+    } catch (IOException e) {
+      System.out.println("파일 생성 실패. 프로그램을 종료합니다.");
+      throw new RuntimeException(e);
+    }
   }
-
-  /**
-   * 소스코드 폴더를 생성합니다.
-   *
-   * @param directory 생성할 폴더
-   * @throws IOException 폴더 생성 실패
-   */
-  void createDirectory(String directory) throws IOException;
 
   /**
    * 파일을 읽어서 String 반환합니다.
