b406988f571235812b29ef86f810f95d116dfc5d
penekhun <penekhun@gmail.com>
2024-07-13 22:50:13 +0900

    test: TestHelper E2E 테스트 작성

--- src/test/java/kr/huni/DynamicCodeCompileSupporter.java
+++ src/test/java/kr/huni/DynamicCodeCompileSupporter.java
@@ -1,10 +1,13 @@
 package kr.huni;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.regex.Pattern;
+
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 
@@ -25,22 +28,78 @@ public class DynamicCodeCompileSupporter {
   }
 
 
-  public static void runHelper(String code, OutputStream outputStream, Object o) {
+  public static boolean testRunWell(String mainCode, String testCode, String expectedOutput)
+      throws InterruptedException, IOException {
     // 임시 파일명으로 바껴서 컴파일이 되지 않기 때문에, 클래스 스코프를 제거해 주었음.
-    code = code.replace("public class", "class");
-    try {
-      Path javaFilePath = Files.createTempFile("test", ".java");
-      Files.write(javaFilePath, code.getBytes(), StandardOpenOption.CREATE);
+    mainCode = mainCode.replace("public class", "class");
+    testCode = testCode.replace("public class", "class");
 
-      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-      int compilationResult = compiler.run(null, outputStream, null,
-          javaFilePath.toFile().getAbsolutePath());
-      if (compilationResult == 0) {
-        Runtime.getRuntime().exec("java -cp " + javaFilePath.getParent().toFile().getAbsolutePath()
-            + " TestHelper");
+    String mergedCode = mergeCode(mainCode, testCode);
+    Path tempPath = Files.createDirectories(Path.of("build/tmp"));
+    Path mergedFile = Files.createTempFile(tempPath, "main", ".java");
+    Files.write(mergedFile, mergedCode.getBytes(), StandardOpenOption.CREATE);
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    assert compiler.run(null, outputStream, null, mergedFile.toFile().getAbsolutePath()) == 0 : "컴파일 에러";
+
+    ProcessBuilder processBuilder = new ProcessBuilder(
+        "java", "-cp", tempPath.toString(), "TestHelper");
+    processBuilder.redirectErrorStream(true);
+    Process process = processBuilder.start();
+
+    ByteArrayOutputStream processOutput = new ByteArrayOutputStream();
+    process.getInputStream().transferTo(processOutput);
+    process.waitFor();
+
+    String output = removeAnsiEscapeCodes(processOutput.toString().stripTrailing());
+    System.out.println("Process output: " + output);
+    System.out.println("Expected output: " + expectedOutput.stripTrailing());
+
+    return output.contains(expectedOutput.stripTrailing());
+  }
+
+  private static String removeAnsiEscapeCodes(String input) {
+    return Pattern.compile("\u001B\\[[;\\d]*m").matcher(input).replaceAll("");
+  }
+
+  /**
+   * Main Code와 Test Code를 합쳐서 반환한다. 합칠때 import 문을 정렬하여 반환한다.
+   */
+  private static String mergeCode(String mainCode, String testCode) {
+    String[] mainLines = mainCode.split("\n");
+    String[] testLines = testCode.split("\n");
+
+    StringBuilder mergedCode = new StringBuilder();
+
+    for (String line : testLines) {
+      if (line.startsWith("import ")) {
+        mergedCode.append(line).append("\n");
       }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
+
+    for (String line : mainLines) {
+      if (line.startsWith("import ")) {
+        mergedCode.append(line).append("\n");
+      }
+    }
+
+    mergedCode.append("\n");
+
+    for (String line : testLines) {
+      if (!line.startsWith("import ")) {
+        mergedCode.append(line).append("\n");
+      }
+    }
+
+    mergedCode.append("\n");
+
+    for (String line : mainLines) {
+      if (!line.startsWith("import ")) {
+        mergedCode.append(line).append("\n");
+      }
+    }
+
+    return mergedCode.toString();
   }
 }
