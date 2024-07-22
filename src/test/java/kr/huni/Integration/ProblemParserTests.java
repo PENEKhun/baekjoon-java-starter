02bad6171b73967b73b57df24f9723b366331233
penekhun <penekhun@gmail.com>
2024-07-22 17:45:55 +0900

    refactor: BaekjoonProblemParser and JsoupWebParser
    BojStarter 생성시 ProblemParser에서 파싱할 문제번호를 바로 초기화하지 않고, 상위 객체에서 세터로 지연초기화 하도록 변경했습니다.

--- src/test/java/kr/huni/Integration/ProblemParserTests.java
+++ src/test/java/kr/huni/Integration/ProblemParserTests.java
@@ -1,38 +1,31 @@
 package kr.huni.Integration;
 
-import kr.huni.problem_parser.JsoupWebParser;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import kr.huni.problem_parser.JsoupWebParser;
 
 @DisplayName("JsoupWebParser 는")
 class ProblemParserTests {
 
   @Test
-  @DisplayName("문제 번호가 존재 할 경우 잘 생성된다.")
-  void notThrowing() {
+  @DisplayName("setProblemNumber 가 잘 동작한다.")
+  void test() throws NoSuchFieldException, IllegalAccessException {
     // given
-    int problemNumber = 1000;
+    JsoupWebParser jsoupWebParser = new JsoupWebParser();
 
-    // when & then
-    Assertions.assertDoesNotThrow(
-        () -> new JsoupWebParser(problemNumber)
-    );
+    // when
+    jsoupWebParser.setProblemNumber(1000);
+
+    // then
+    Class<?> clazz = jsoupWebParser.getClass();
+    Field field = clazz.getDeclaredField("problemNumber");
+    field.setAccessible(true);
+    int problemNumber = (int) field.get(jsoupWebParser);
+    assertEquals(1000, problemNumber);
   }
-
-  @Test
-  @DisplayName("문제 번호가 존재하지 않을 경우 예외를 던진다. = 생성되지 않는다.")
-  void invalid_problem_throwing() {
-    // given
-    int problemNumber = -1;
-
-    // when & then
-    Assertions.assertThrows(
-        IllegalArgumentException.class,
-        () -> new JsoupWebParser(problemNumber),
-        "문제를 찾을 수 없습니다. 올바른 문제 번호를 입력해주세요."
-    );
-  }
-
-
 }
